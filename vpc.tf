b7a27c95b5aae3beda933490aeeefb1bf77738a6
MAC <mac@MACs-MacBook-Pro.local>
2025-08-06 14:01:40 +0100

    terraform eks

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,13 @@
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
+  profile = "default"
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "ezzy-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -18,7 +19,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "ezzy-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
